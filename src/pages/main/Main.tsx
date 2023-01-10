3328fe44d5c20f47ec258a686b98230f8fb6f9d1
1995elantra <lhkjoa@naver.com>
2023-01-11 02:46:15 +0900

    added src/pages/main edited Router.tsx added iconhouse.png in assets added src/pages/main edited Router.tsx :wq!
    :wq!
    :wq!
    
    ;wq
    i
    a
    o

---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,23 @@
+import * as S from './Main.styled';
+import Map from './Map';
+import hasukLogo from '../../assets/iconhouse.png';
+function Main() {
+  const onchange = () => {
+    console.log();
+  };
+  return (
+    <S.Container>
+      <S.Header>
+        <img src={hasukLogo} alt="하숙" />
+        <h1>hasuk</h1>
+        <select onChange={onchange}>
+          <option value="고려대">고려대</option>
+        </select>
+      </S.Header>
+      <S.Wrapper>
+        <Map />
+      </S.Wrapper>
+    </S.Container>
+  );
+}
+export default Main;
