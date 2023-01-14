be7d3affe82933333222a3c4b593bccf5b21d37f
1995elantra <lhkjoa@naver.com>
2023-01-14 12:57:32 +0900

    임시

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -2,14 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 import Houses from './houses/Houses';
 import Focused from './main/Focused';
 import Main from './main/Main';
-import House from './houses/house/House';
+//import House from './houses/house/House';
 import NotFound from './NotFound';
 
 function Router() {
   return (
     <Routes>
       <Route path="/houses/:region_id" element={<Houses />} />
-      <Route path="/house/:house_id" element={<House />} />
+      {/* <Route path="/house/:house_id" element={<House />} /> */}
       <Route path="/main" element={<Main />} />
       <Route path="/main/:focused" element={<Focused />} />
       <Route path="/*" element={<NotFound />} />
